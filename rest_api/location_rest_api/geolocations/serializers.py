873c5371720f2eea81ab532ce4aeb7cb3f2b0ee9
johnedstone <johnedstone@gmail.com>
2022-06-06 00:25:04 -0400

    does create work?

--- rest_api/location_rest_api/geolocations/serializers.py
+++ rest_api/location_rest_api/geolocations/serializers.py
@@ -5,11 +5,19 @@ from telegram_users.models import TelegramUser
 #class GeolocationSerializer(serializers.HyperlinkedModelSerializer):
 class GeolocationSerializer(serializers.ModelSerializer):
 
+    """
     def to_internal_value(self, data):
         user_id = data.get('telegram_user')
         _ = TelegramUser.objects.get_or_create_telegram_user(user_id=user_id)
 
         return data
+    """
+
+    def create(self, validated_data):
+        user_id = data.get('telegram_user')
+        _ = TelegramUser.objects.get_or_create_telegram_user(user_id=user_id)
+
+        return Geolocation.objects.create(**validated_data)
 
     class Meta:
         fields = ('telegram_user', 'longitude', 'latitude', 'accuracy_radius')
