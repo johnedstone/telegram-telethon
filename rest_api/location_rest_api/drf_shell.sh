51c262d8a6f32339428d25e7a5a7a22ab496ba36
johnedstone <johnedstone@gmail.com>
2022-06-06 20:16:12 -0400

    added validation to def to_internal_value

--- rest_api/location_rest_api/drf_shell.sh
+++ rest_api/location_rest_api/drf_shell.sh
@@ -3,11 +3,14 @@ from geolocations.serializers import GeolocationSerializer
 import io
 from rest_framework.parsers import JSONParser
 
-content = b'{"telegram_user": 6953 , "longitude": 7.4, "latitude": 9.5, "accuracy_radius": 5}'
+#content = b'{"telegram_user": 6953 , "longitude": 7.4, "latitude": 9.5, "accuracy_radius": 5}'
+content = b'{"telegram_user": 69536953 , "longitude": 7.3, "latitude": 0.0}'
 stream = io.BytesIO(content)
 data = JSONParser().parse(stream)
 gs = GeolocationSerializer(data=data)
 print(gs)
 print(gs.is_valid())
-print(gs.validated_data)
-print(gs.save())
+#print(dir(gs))
+if gs.is_valid():
+    print(gs.validated_data)
+    print(gs.save())
--- rest_api/location_rest_api/geolocations/serializers.py
+++ rest_api/location_rest_api/geolocations/serializers.py
@@ -15,16 +15,38 @@ class GeolocationSerializer(serializers.ModelSerializer):
 
 
     def to_internal_value(self, data):
-        #logger.debug(help(data))
 
-        logging.debug(data)
+        logger.info(data)
+        # Not sure if this validation check is needed
+        for ea in ['telegram_user', 'longitude', 'latitude']:
+            if ea not in data:
+                logger.info(f'{ea} is required.')
+                raise serializers.ValidationError({
+                    ea: f'{ea} is required.'
+                })
+            else:
+                if ea == 'telegram_user':
+                    if not isinstance(data[ea], int):
+                        logger.info(f'{ea} is not an integer. It is a {type(data[ea])}')
+                        raise serializers.ValidationError({
+                            ea: f'{ea} is not an integer. It is a {type(data[ea])}'
+                        })
+                    else:
+                        logger.info(f'{ea} is an integer. It is a {type(data[ea])}')
+                if ea in ['longitude', 'latitude']:
+                    if not isinstance(data[ea], float):
+                        logger.info(f'{ea} is not an float. It is a {type(data[ea])}')
+                        raise serializers.ValidationError({
+                            ea: f'{ea} is not an float'
+                        })
+                    else:
+                        logger.info(f'{ea} is an float. It is a {type(data[ea])}')
+
         user_id = data.pop('telegram_user')
-        logging.debug(data)
 
         telegram_user = TelegramUser.objects.get_or_create_telegram_user(user_id=user_id)
 
         data['telegram_user'] = telegram_user
-        logging.debug(data)
 
         return data
 
