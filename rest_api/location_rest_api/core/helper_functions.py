ca84ed8ef066a1cb10dac65a5c837e2404b9925e
johnedstone <johnedstone@gmail.com>
2022-06-20 15:03:20 -0400

    set up tokens with secrets.token_urlsafe(), and changed get_random_id to secrets.token_urlsafe(12) which is 16 characters long (as before) Note: secrets.token_urlsafe() is 43 characters long, which we are using here for the SECRET KEY, whereas the default is 50 characters - see https://humberto.io/blog/tldr-generate-django-secret-key/

--- rest_api/location_rest_api/core/helper_functions.py
+++ rest_api/location_rest_api/core/helper_functions.py
@@ -12,14 +12,22 @@ def get_random_id():
     Checks first to make sure that the random_id does not already exist
     as 'randominzed_id'.  If so, try again, until a unique random_id is
     generated.
+
+    See also: https://humberto.io/blog/tldr-generate-django-secret-key/
+    >>> secrets.token_urlsafe(12)
+    '9kOQ6M_r9k0MHnps'
+    >>> len(secrets.token_urlsafe(12))
+    16
     """
 
     not_unique = True
 
     while not_unique:
-        random_id = ''.join(secrets.choice(
-               string.ascii_uppercase.replace('O', '').replace('I', '') + \
-                       string.digits.replace('0', '').replace('1', '')) for i in range(16))
+        #random_id = ''.join(secrets.choice(
+        #       string.ascii_uppercase.replace('O', '').replace('I', '') + \
+        #               string.digits.replace('0', '').replace('1', '')) for i in range(16))
+        random_id = secrets.token_urlsafe(12)
+
         try:
             _ = tu_models.TelegramUser.objects.get(randomized_id__exact=random_id)
             logger.info('Exists! Try again!! Get another random id')
