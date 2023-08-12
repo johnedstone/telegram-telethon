402012e914c68e050a0374ce2e54ce72887e235c
johnedstone <johnedstone@gmail.com>
2023-08-11 22:13:09 -0400

    updated ordering of geolocation admin

--- rest_api/location_rest_api/geolocations/admin.py
+++ rest_api/location_rest_api/geolocations/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Geolocation
 
 class GeolocationAdmin(admin.ModelAdmin):
-    list_display = ('telegram_user', 'longitude', 'latitude', 'created_at',
+    list_display = ('telegram_user',
+            'latitude',
+            'longitude',
+            'created_at',
             'bot', 'heading', 'period')
     list_filter = ('telegram_user__user_id', 'telegram_user__username') #, 'bot')
 
